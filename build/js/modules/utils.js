62a019d5fcb6e5e6f47a373ce4143a0b1e5f58a3
victoria-nsl <vbnaish@yandex.ru>
2022-02-08 17:23:51 +0300

    Добавляет верстку мобильной версии шапки

---
+++ build/js/modules/utils.js
@@ -0,0 +1,20 @@
+const KEYCODE_TAB = 9;
+
+const setFocusTab = (evt, firstElement, lastElement) => {
+  const isShiftPressed = evt.shiftKey;
+  const isTabPressed = (evt.key === 'Tab' || evt.keyCode === KEYCODE_TAB);
+  if (!isTabPressed) {
+    return;
+  }
+
+  if (isShiftPressed && isTabPressed && document.activeElement === firstElement) {
+    evt.preventDefault();
+    lastElement.focus();
+  }
+
+  if (!isShiftPressed && isTabPressed && document.activeElement === lastElement) {
+    evt.preventDefault();
+    firstElement.focus();
+  }
+};
+export {setFocusTab};
