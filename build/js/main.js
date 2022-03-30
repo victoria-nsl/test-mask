8d43a233eeaad79800e48a5f630df7b150579b4a
victoria-nsl <vbnaish@yandex.ru>
2022-03-30 16:21:23 +0300

    Добавлет верстку шапки для десктопной версии

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,1 @@
 import './modules/utils.js';
-import './modules/navigation.js';
-import './modules/mask-phone.js';
-import './modules/cart.js';
-import './modules/select.js';
-import './modules/form-send.js';
