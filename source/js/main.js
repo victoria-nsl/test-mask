8ac9102df9f88c2b851c5151005f71de78ffd67b
victoria-nsl <vbnaish@yandex.ru>
2022-03-30 10:57:29 +0300

    Начальное состояние проекта

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,2 @@
 import './modules/utils.js';
-import './modules/navigation.js';
-import './modules/mask-phone.js';
-import './modules/cart.js';
-import './modules/select.js';
-import './modules/form-send.js';
+
