cff7a4abfc5b744512a1f83999f43834a549d400
victoria-nsl <vbnaish@yandex.ru>
2022-03-31 11:18:08 +0300

    Добавляет стайлгад для шапки проекта

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,4 +1,17 @@
 const header = document.querySelector('.page-header');
+const page = document.body;
+
+const closeMenu = () => {
+  header.classList.remove('page-header--opened');
+  header.classList.add('page-header--closed');
+  page.classList.remove('page-body--no-scroll');
+};
+
+const openMenu = () => {
+  header.classList.remove('page-header--closed');
+  header.classList.add('page-header--opened');
+  page.classList.add('page-body--no-scroll');
+};
 
 if (header && document.documentElement.clientWidth < 1023) {
   const buttonToggle = header.querySelector('.page-header__button');
@@ -7,12 +20,10 @@ if (header && document.documentElement.clientWidth < 1023) {
 
   const toggleMenu = () => {
     if (header.classList.contains('page-header--closed')) {
-      header.classList.remove('page-header--closed');
-      header.classList.add('page-header--opened');
-    } else {
-      header.classList.remove('page-header--opened');
-      header.classList.add('page-header--closed');
+      openMenu();
+      return;
     }
+    closeMenu();
   };
 
   buttonToggle.addEventListener('click', toggleMenu);
